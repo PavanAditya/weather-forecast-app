462ae88e7cf17a0bb3bd3886cae7e473b92cf6cf
Pavanaditya Mylavarapu <M1050962@mindtree.com>
2019-12-12 03:36:49 +0530

    weather app display styles modified

--- src/components/WeatherApp.vue
+++ src/components/WeatherApp.vue
@@ -51,6 +51,11 @@ main {
   background-size: cover;
   background-repeat: no-repeat;
 
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+
   box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
 }
 
