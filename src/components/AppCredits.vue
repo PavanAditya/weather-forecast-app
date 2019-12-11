cd078e475eb5ebb64376c2813a92182e1bf40404
Pavanaditya Mylavarapu <M1050962@mindtree.com>
2019-12-12 03:39:01 +0530

    Footer Credits added

--- src/components/AppCredits.vue
+++ src/components/AppCredits.vue
@@ -1,15 +1,41 @@
 <template>
-  <div>
-
-  </div>
+  <footer>
+    <small>
+      A Project by
+      <b>
+        <a href="https://pavanaditya.com" target="_blank" rel="noopener">
+          <cite>Pavan Aditya M S</cite>
+        </a>
+      </b>
+    </small>
+  </footer>
 </template>
 
 <script>
-  export default {
+export default {
+  name: 'AppCredits',
 
+  props: {
+    year: {
+      type: Number,
+      required: true
+    },
+    blank: ' '
   }
+}
 </script>
 
 <style lang="scss" scoped>
+footer {
+  text-align: center;
+  position: absolute;
+  bottom: 10px;
+  visibility: hidden;
+}
 
+@media screen and (min-width: 450px) {
+  footer {
+    visibility: visible;
+  }
+}
 </style>
